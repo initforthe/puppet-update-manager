9516820807f37ad1798a24ecd13c55db0fe6eae5
Tom Simnett <tom@initforthe.com>
2014-10-15 15:51:55 +0100

    Allow choice over what updates to show

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,13 @@
 # Basic update manager configuration
-class update_manager {
+class update_manager (
+  $ensure = present,
+  $prompt = 'lts', # valid options are 'never', 'normal', 'lts'
+) {
+  file { '/etc/update-manager/release-upgrades':
+    ensure  => $ensure,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('update_manager/release-upgrades.erb'),
+  }
 }
